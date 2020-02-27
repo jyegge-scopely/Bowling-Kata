c9b5f91392c49d02692342f4e6c73ac7187d7548
Jason Yegge <jason.yegge@scopely.com>
2020-02-27 16:59:25 -0700

    Project setup.

---
+++ Bowling-Kata.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bowling_Kata.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Bowling-Kata/Bowling.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Bowling_Kata
+{
+    public class Bowling
+    {
+    }
+}
